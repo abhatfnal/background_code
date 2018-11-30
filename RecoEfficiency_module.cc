41c0f7643daca1a817b6a4b8df3f21eeff3c68f3
abhatfnal <31660280+abhatfnal@users.noreply.github.com>
2018-11-30 08:33:54 -0600

    Add files via upload
    Saving track information and match information to TTrees

--- RecoEfficiency_module.cc
+++ RecoEfficiency_module.cc
@@ -46,6 +46,8 @@ using namespace std;
 #include "nusimdata/SimulationBase/MCParticle.h"
 #include "nusimdata/SimulationBase/MCTruth.h"
 #include "nusimdata/SimulationBase/MCFlux.h"
+#include "lardataobj/RecoBase/Vertex.h"
+
 
 
 // ROOT
@@ -91,19 +93,46 @@ private:
     
     // Declare member data here.
     
+    Int_t fEvent;
     
     std::string fMC_track_tag,fReco_track_tag,fHit_tag,fMatch_tag;
     
-    //  Int_t pdg;
-    Double_t X_reco=0.0;
+    
+    
+    Double_t MC_Track_StartX_det=0.0;
+    Double_t MC_Track_StartY_det=0.0;
+    Double_t MC_Track_StartZ_det=0.0;
+    Double_t MC_Track_EndX_det=0.0;
+    Double_t MC_Track_EndY_det=0.0;
+    Double_t MC_Track_EndZ_det=0.0;
+    Double_t MC_Track_Length=0.0;
+    Double_t XZangle=0.0;
+    Double_t Yangle=0.0;
+    
+    
+    
+    Double_t Reco_Track_StartX=0.0;
+    Double_t Reco_Track_StartY=0.0;
+    Double_t Reco_Track_StartZ=0.0;
+    Double_t Reco_Track_EndX=0.0;
+    Double_t Reco_Track_EndY=0.0;
+    Double_t Reco_Track_EndZ=0.0;
+    Double_t Reco_Track_Length=0.0;
+    
+    Double_t Reco_Track_StartX_match=0.0;
+    Double_t Reco_Track_StartY_match=0.0;
+    Double_t Reco_Track_StartZ_match=0.0;
+    Double_t Reco_Track_EndX_match=0.0;
+    Double_t Reco_Track_EndY_match=0.0;
+    Double_t Reco_Track_EndZ_match=0.0;
+    Double_t Reco_Track_Length_match=0.0;
+    
     
     
     art::ServiceHandle<art::TFileService> tfs;
     
-    //   TTree *Matchtree = new TTree("Matchtree","Matchtree");
-    
-    
-    
+    TTree *Tracktree = new TTree("Tracktree","Tracktree");
+    TTree *Matchtree = new TTree("Matchtree","Matchtree");
 };
 
 
@@ -126,10 +155,11 @@ RecoEfficiency::RecoEfficiency(fhicl::ParameterSet const & p)
 
 void RecoEfficiency::analyze(art::Event const & e)
 {
-    
+    fEvent= e.id().event();
     // Implementation of required member function here.
     art::Handle<std::vector<sim::MCTrack> > mctrack_handle;
     e.getByLabel(fMC_track_tag,mctrack_handle);
+    if (!mctrack_handle.isValid()) return;
     
     art::Handle<std::vector<recob::Track> > recotrack_handle;
     e.getByLabel(fReco_track_tag,recotrack_handle);
@@ -141,36 +171,65 @@ void RecoEfficiency::analyze(art::Event const & e)
     
     art::FindMany<simb::MCParticle,anab::BackTrackerHitMatchingData> backtrack_handle(hit_handle,e,fMatch_tag);
     
+    cout <<"Event: "<<fEvent<<endl;
+    
     //   LoadVertex(e.run(),e.event());
     
     auto const& mctrack_vec(*mctrack_handle);
     
+    auto const& recotrack_vec(*recotrack_handle);
     
     Int_t mctrackcounter=0;
     
     
-    
-    for (size_t i_c = 0, size_mctrackvec = mctrack_handle->size(); i_c != size_mctrackvec; ++i_c) { //START MCTRACK FOR LOOP
+    for (size_t i_c = 0; i_c < mctrack_vec.size(); ++i_c) { //START MCTRACK FOR LOOP
         
         auto pdg=mctrack_vec[i_c].PdgCode();
-        
-        if (mctrack_handle->size() <0 || abs(pdg)!=13)
-            continue;
-        
-        auto mctrackid = mctrack_vec[i_c].TrackID();
-        
+
+      if (mctrack_vec.size() <2 || abs(pdg)!=13)
+          continue;
+
         mctrackcounter++;
+        auto mctrackid=mctrack_vec[i_c].TrackID();
         
         
-        cout<<"MCTrack Counter: "<<mctrackcounter<<endl;
+        //     cout<<"MCTrack Counter: "<<mctrackcounter<<endl;
         //   cout<<"MC TRACK ID: "<<mctrackid<<endl;
+   /*
+        MC_Track_StartX_det=mctrack_vec[i_c].at(0).X();
+        MC_Track_StartY_det=mctrack_vec[i_c].at(0).Y();
+        MC_Track_StartZ_det=mctrack_vec[i_c].at(0).Z();
         
+        MC_Track_EndX_det=mctrack_vec[i_c].at(mctrack_vec.size()-1).X();
+        MC_Track_EndY_det=mctrack_vec[i_c].at(mctrack_vec.size()-1).Y();
+        MC_Track_EndZ_det=mctrack_vec[i_c].at(mctrack_vec.size()-1).Z();
+   */
+        MC_Track_Length=sqrt(pow((MC_Track_EndX_det-MC_Track_StartX_det),2)+pow((MC_Track_EndY_det-MC_Track_StartY_det),2)+pow((MC_Track_EndZ_det-MC_Track_StartZ_det),2));
         
+        XZangle=((MC_Track_EndX_det-MC_Track_StartX_det)/(MC_Track_EndZ_det-MC_Track_StartZ_det));
+        
+        Yangle=(sqrt(pow((MC_Track_EndZ_det-MC_Track_StartZ_det),2)+pow((MC_Track_EndX_det-MC_Track_StartX_det),2))/(MC_Track_EndY_det-MC_Track_StartY_det));
+        
+        Double_t ratio_largest=0.0;
         Int_t recotrackcounter=0;
-        
-        for (size_t i_t = 0, size_track = recotrack_handle->size(); i_t != size_track; ++i_t) {//START RECOTRACK FOR LOOP
+        Int_t recotrackcounter_largestratio=0;
+        if (recotrack_handle->size()==0)
+        {cout<<"*************ZERO RECO TRACKS FOUND"<<endl;}
+        for (size_t i_t = 0; i_t < recotrack_vec.size(); ++i_t) {//START RECOTRACK FOR LOOP
             recotrackcounter++;
-            cout<<"RecoTrack Counter: "<<recotrackcounter<<endl;
+            //cout<<"RecoTrack Counter: "<<recotrackcounter<<endl;
+            
+            Reco_Track_StartX= recotrack_vec[i_t].Vertex().X();
+            
+             Reco_Track_StartY= recotrack_vec[i_t].Vertex().Y();
+             Reco_Track_StartZ= recotrack_vec[i_t].Vertex().Z();
+             
+             Reco_Track_EndX=recotrack_vec[i_t].End().X();
+             Reco_Track_EndY=recotrack_vec[i_t].End().Y();
+             Reco_Track_EndZ=recotrack_vec[i_t].End().Z();
+             
+             Reco_Track_Length=sqrt(pow((Reco_Track_EndX-Reco_Track_StartX),2)+pow((Reco_Track_EndY-Reco_Track_StartY),2)+pow((Reco_Track_EndZ-Reco_Track_StartZ),2));
+             
             
             const std::vector<art::Ptr<recob::Hit> > hit_v = track_hit_assn_v.at(i_t);
             
@@ -198,26 +257,39 @@ void RecoEfficiency::analyze(art::Event const & e)
                     if (abs(pdg_particle)!=13 || (int)mcparticleid!=(int)mctrackid || match_vec[i_p]->isMaxIDE!=1  )
                         continue;
                     
-                    //      cout<<"PARTICLE ID: "<<mcparticleid<<endl;
-                    //       cout<<"MC TRACK ID: "<<mctrackid<<endl;
-              //      if (match_vec[i_p]->isMaxIDE==1){
-                        backtrackedhitcounter++;
-              //      }
+                    //          cout<<"PARTICLE ID: "<<mcparticleid<<endl;
+                    //          cout<<"MC TRACK ID: "<<mctrackid<<endl;
+                    
+                    backtrackedhitcounter++;
+                    
                     
                 }//END MC PARTICLE FOR LOOP
             }//END HIT FOR LOOP
-         //   cout<<"hitcounter: "<<hitcounter<<endl;
-         //   cout<<"backtrackedhitcounter: "<<backtrackedhitcounter<<endl;
+            //   cout<<"hitcounter: "<<hitcounter<<endl;
+            //   cout<<"backtrackedhitcounter: "<<backtrackedhitcounter<<endl;
             ratio=backtrackedhitcounter/hitcounter;
-            if (ratio>0){
-            cout<<"**********************************ratio: "<<ratio<<endl;
+            if (ratio>ratio_largest){
+                recotrackcounter_largestratio=recotrackcounter;
+                ratio_largest=ratio;
+                /*
+                 Reco_Track_StartX_match=Reco_Track_StartX;
+                 Reco_Track_StartY_match=Reco_Track_StartY;
+                 Reco_Track_StartZ_match=Reco_Track_StartZ;
+                 Reco_Track_EndX_match=Reco_Track_EndX;
+                 Reco_Track_EndY_match=Reco_Track_EndY;
+                 Reco_Track_EndZ_match=Reco_Track_EndZ;
+                 Reco_Track_Length_match=Reco_Track_Length;
+                 */
+                cout<<"**********************************ratio: "<<ratio<<endl;
             }
+            
+            //         Matchtree->Fill();
+            
         }//END RECO TRACK FOR LOOP
-        
-        //  cout<<"Smallest Distance: "<<distance_smallest<<endl;
-        //    cout<<"******Best Match for Cluster : "<<mctrackcounter<<" is reco track number: "<<trackcounter_smallest<<" with distance:  "<<distance_smallest<<"*********"<<endl;
-        //     cout<<"hitcounter6: "<<hitcounter<<endl;
-        //     cout<<"backtrackedhitcounter5: "<<backtrackedhitcounter<<endl;
+        if (ratio_largest==0){
+            cout<<"MC TRACK # "<<mctrackcounter<<" DOES NOT have a RECO TRACK match."<<endl;
+        }
+        else {cout<<"MC TRACK # "<<mctrackcounter<<" matches RECO TRACK # "<<recotrackcounter_largestratio<<" with RATIO: "<<ratio_largest<<endl;}
         /*
          
          for (auto const& hit : hits) {//START CLUSTER HIT LOOP
@@ -228,9 +300,9 @@ void RecoEfficiency::analyze(art::Event const & e)
          }//END CLUSTER HIT LOOP
          
          
-         
-         Matchtree->Fill();
          */
+        //     Tracktree->Fill();
+
     }//END MCTRACK FOR LOOP
     
     
@@ -256,6 +328,30 @@ void RecoEfficiency::beginJob()
      //   Matchtree->Branch("pointdistance_smallest",&pointdistance_smallest,"pointdistance_smallest/D");
      Matchtree->Branch("distance_smallest",&distance_smallest,"distance_smallest/D");
      */
+    art::ServiceHandle<art::TFileService> tfs;
+    Tracktree->Branch("MC_Track_StartX_det",&MC_Track_StartX_det,"MC_Track_StartX_det/D");
+    Tracktree->Branch("MC_Track_StartY_det",&MC_Track_StartY_det,"MC_Track_StartY_det/D");
+    Tracktree->Branch("MC_Track_StartZ_det",&MC_Track_StartZ_det,"MC_Track_StartZ_det/D");
+    Tracktree->Branch("MC_Track_EndX_det",&MC_Track_EndX_det,"MC_Track_EndX_det/D");
+    Tracktree->Branch("MC_Track_EndY_det",&MC_Track_EndY_det,"MC_Track_EndY_det/D");
+    Tracktree->Branch("MC_Track_EndZ_det",&MC_Track_EndZ_det,"MC_Track_EndZ_det/D");
+    Tracktree->Branch("XZangle",&XZangle,"XZangle/D");
+    Tracktree->Branch("Yangle",&Yangle,"Yangle/D");
+    Tracktree->Branch("Reco_Track_StartX",&Reco_Track_StartX,"Reco_Track_StartX/D");
+    Tracktree->Branch("Reco_Track_StartY",&Reco_Track_StartY,"Reco_Track_StartY/D");
+    Tracktree->Branch("Reco_Track_StartZ",&Reco_Track_StartZ,"Reco_Track_StartZ/D");
+    Tracktree->Branch("Reco_Track_EndX",&Reco_Track_EndX,"Reco_Track_EndX/D");
+    Tracktree->Branch("Reco_Track_EndY",&Reco_Track_EndY,"Reco_Track_EndY/D");
+    Tracktree->Branch("Reco_Track_EndZ",&Reco_Track_EndZ,"Reco_Track_EndZ/D");
+    
+    
+    Matchtree->Branch("Reco_Track_StartX_match",&Reco_Track_StartX_match,"Reco_Track_StartX_match/D");
+    Matchtree->Branch("Reco_Track_StartY_match",&Reco_Track_StartY_match,"Reco_Track_StartY_match/D");
+    Matchtree->Branch("Reco_Track_StartZ_match",&Reco_Track_StartZ_match,"Reco_Track_StartZ_match/D");
+    Matchtree->Branch("Reco_Track_EndX_match",&Reco_Track_EndX_match,"Reco_Track_EndX_match/D");
+    Matchtree->Branch("Reco_Track_EndY_match",&Reco_Track_EndY_match,"Reco_Track_EndY_match/D");
+    Matchtree->Branch("Reco_Track_EndZ_match",&Reco_Track_EndZ_match,"Reco_Track_EndZ_match/D");
+    
     
 }
 
